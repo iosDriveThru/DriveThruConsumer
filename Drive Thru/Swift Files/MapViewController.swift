a558a144d0d158b21705e2c8e17e7ed1c5f84607
Nanite <Praveen@Nanites-MacBook-Pro.local>
2016-03-17 12:05:29 +0530

    yogi commit

--- Drive Thru/Swift Files/MapViewController.swift
+++ Drive Thru/Swift Files/MapViewController.swift
@@ -63,6 +63,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         }
         
         //  setupGeoFireForCurrentLocation(currentLatitude, lng: currentLongitude)
+        // test
     }
     
     override func didReceiveMemoryWarning() {
